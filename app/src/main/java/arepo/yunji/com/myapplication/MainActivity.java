5424f3efd2f1df1395893466a88cfe0da33ecdfd
yangzhou <yangzhou@yunjiglobal.com>
2019-06-21 15:54:24 +0800

    更新文件

--- app/src/main/java/arepo/yunji/com/myapplication/MainActivity.java
+++ app/src/main/java/arepo/yunji/com/myapplication/MainActivity.java
@@ -6,8 +6,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import arepo.yunji.com.common.RepoLibUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,27 +27,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        TextView textView = findViewById(R.id.txt);
+        textView.setText("Value:" + RepoLibUtils.getLibStr());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
